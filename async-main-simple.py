873935995d047aa8ffea3ed5184e624eae419029
Rob Koch <rob.koch@fvfhh4laq6lt.home>
2023-09-05 15:27:33 -0700

    update for python 3.11

--- async-main-simple.py
+++ async-main-simple.py
@@ -1,38 +1,43 @@
 import asyncio
 import os
 
-from datetime import timedelta
-from momento import CacheClientAsync, Configurations, CredentialProvider
-from momento.responses import CacheGet, CacheSet, CreateCache, ListCaches
+import momento.aio.simple_cache_client as scc
+import momento.errors as errors
 
 from pymongo import MongoClient
 import json 
 import time
 from bson.json_util import dumps, loads
 
-# _MOMENTO_AUTH_TOKEN = os.getenv("MOMENTO_AUTH_TOKEN")
-_MOMENTO_AUTH_TOKEN = CredentialProvider.from_environment_variable("MOMENTO_AUTH_TOKEN")
+_MOMENTO_AUTH_TOKEN = os.getenv("MOMENTO_AUTH_TOKEN")
 _CACHE_NAME = "cache"
-_ITEM_DEFAULT_TTL_SECONDS = timedelta(seconds=60)
+_ITEM_DEFAULT_TTL_SECONDS = 60
 _KEY = os.getenv("KEY")
 _SKIPCACHE = os.getenv("SKIP_CACHE")
 
 client = MongoClient('mongodb+srv://not-my-username:not-my-password@cluster0.oko2ogn.mongodb.net/test')
 
-async def _create_cache(cache_client: CacheClientAsync, cache_name: str) -> None:
+async def _create_cache(cache_client: scc.SimpleCacheClient, cache_name: str) -> None:
     try:
         await cache_client.create_cache(cache_name)
-    except CreateCache.CacheAlreadyExists():
+    except errors.AlreadyExistsError:
         print(f"Cache with name: {cache_name!r} already exists.")
 
-async def _list_caches(cache_client: CacheClientAsync) -> None:
+
+async def _list_caches(cache_client: scc.SimpleCacheClient) -> None:
     print("Listing caches:")
-    list_caches_response = await cache_client.list_caches()
-    print (list_caches_response)
+    list_cache_result = await cache_client.list_caches()
+    while True:
+        for cache_info in list_cache_result.caches():
+            print(f"- {cache_info.name()!r}")
+        next_token = list_cache_result.next_token()
+        if next_token is None:
+            break
+        list_cache_result = await cache_client.list_caches(next_token)
     print("")
 
 async def main() -> None:
-    async with await CacheClientAsync.create(Configurations.Laptop.v1(), _MOMENTO_AUTH_TOKEN, _ITEM_DEFAULT_TTL_SECONDS) as cache_client:
+    async with scc.init(_MOMENTO_AUTH_TOKEN, _ITEM_DEFAULT_TTL_SECONDS) as cache_client:
         await _create_cache(cache_client, _CACHE_NAME)
         await _list_caches(cache_client)
         skip_cache = _SKIPCACHE=="True"
@@ -46,23 +51,18 @@ async def main() -> None:
                 json_data = dumps(result) 
                 print(f"Uncached item (cut-off at 100 chars): {json_data[0:100]!r}")
             else:
-                get_response = await cache_client.get(_CACHE_NAME, _KEY)
-                match get_response:
-                    case CacheGet.Hit() as hit:
-                        print(f"Look up resulted in a: {hit}")
-                        print(f"Getting Key: {_KEY!r}")
-                        print(f"Looked up Value (cut-off at 100 chars): {hit}")
-                    case CacheGet.Miss():
-                        # print("Look up resulted in a: miss. This is unexpected.")
-                        result = await get_results()
-                        json_data = dumps(result) 
-                        print(f"Setting Key: {_KEY!r}")
-                        await cache_client.set(_CACHE_NAME, _KEY, json_data)
-                        print(f"Value stored in Momento (cut-off at 100 chars): {json_data[0:100]!r}")
-                    case CacheGet.Error() as error:
-                        print(f"Error getting cache: {error.message}")
-                    case _:
-                        print("Unreachable")
+                get_resp = await cache_client.get(_CACHE_NAME, _KEY)
+                if str(get_resp.status()) == 'CacheGetStatus.HIT':
+                    print(f"Look up resulted in a: {str(get_resp.status())}")
+                    print(f"Getting Key: {_KEY!r}")
+                    get_resp = await cache_client.get(_CACHE_NAME, _KEY)
+                    print(f"Looked up Value (cut-off at 100 chars): {get_resp.value()[0:100]!r}")
+                else:
+                    result = await get_results()
+                    json_data = dumps(result) 
+                    print(f"Setting Key: {_KEY!r}")
+                    await cache_client.set(_CACHE_NAME, _KEY, json_data)
+                    print(f"Value stored in Momento (cut-off at 100 chars): {json_data[0:100]!r}")
             executionTime = (time.time() - loopStartTime)
             results_dict[x] = executionTime
         # finally
